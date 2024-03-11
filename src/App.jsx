61e9d1e79e33e57bbe772581f0d454e0f27c4dd2
Kartikey Jaiswal <karan74jaiswal@gmail.com>
2024-03-12 00:01:54 +0530

    implemented Categories Component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Categories from "./Categories";
+import Navbar from "./Navbar";
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Categories />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Category.jsx
@@ -0,0 +1,18 @@
+const Category = function ({ category }) {
+  return (
+    <div className="category-container">
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${category.imageUrl})`,
+        }}
+      />
+      <div className="category-body-container">
+        <h2>{category.title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default Category;
