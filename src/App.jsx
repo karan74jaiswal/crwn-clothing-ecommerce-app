7bc1c5b8bb5f0fce7a3767e592ea000c4d1534d5
Kartikey Jaiswal <karan74jaiswal@gmail.com>
2024-03-12 02:41:25 +0530

    Changed Tab Icon and Title, Implemented Sass, Implemented Content of navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
 import Categories from "./Categories";
 import Navbar from "./Navbar";
 function App() {
+  useEffect(() => {
+    let link = document.querySelector("link[rel~='icon']");
+    if (!link) {
+      link = document.createElement("link");
+      link.rel = "icon";
+      document.getElementsByTagName("head")[0].appendChild(link);
+    }
+    link.href = "https://stackoverflow.com/favicon.ico";
+    document.title = "crwn Clothing";
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,3 +1,12 @@
 export default function Navbar() {
-  return;
+  return (
+    <nav>
+      <img src="" alt="crwn-logo" />
+      <div>
+        <p>Shop</p>
+        <p>Sign In</p>
+        <p></p>
+      </div>
+    </nav>
+  );
 }
