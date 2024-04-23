f2f7cc4a179f51352b37bc342474d85b8dec6c6d
Kartikey Jaiswal <karan74jaiswal@gmail.com>
2024-04-24 02:11:52 +0530

    Added Lazy Loading for improving Performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,35 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AppLayout from "./pages/AppLayout";
-import Categories from "./pages/Categories";
-import Signin from "./pages/Signin";
-import Shop from "./pages/Shop";
-import Section from "./pages/Section";
 import { AuthorizationProvider } from "./contexts/AuthorizationContext";
 import { ProductsProvider } from "./contexts/ProductsContext";
 import { CartProvider } from "./contexts/CartContext";
-import Checkout from "./pages/Checkout";
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const Categories = lazy(() => import("./pages/Categories"));
+const Signin = lazy(() => import("./pages/Signin"));
+const Shop = lazy(() => import("./pages/Shop"));
+const Section = lazy(() => import("./pages/Section"));
+const Checkout = lazy(() => import("./pages/Checkout"));
 function App() {
   return (
     <BrowserRouter>
       <AuthorizationProvider>
         <ProductsProvider>
           <CartProvider>
-            <Routes>
-              <Route path="/" element={<AppLayout />}>
-                <Route index element={<Categories />} />
-                <Route path="signin" element={<Signin />} />
-                <Route path="shop" element={<Shop />} />
-                <Route path="shop/:category" element={<Section />} />
-                <Route path="checkout" element={<Checkout />} />
-                <Route
-                  path="*"
-                  element={<h1>Error 404 ----- Page does not exist</h1>}
-                />
-              </Route>
-            </Routes>
+            <Suspense fallback={<h1>...Loading...</h1>}>
+              <Routes>
+                <Route path="/" element={<AppLayout />}>
+                  <Route index element={<Categories />} />
+                  <Route path="signin" element={<Signin />} />
+                  <Route path="shop" element={<Shop />} />
+                  <Route path="shop/:category" element={<Section />} />
+                  <Route path="checkout" element={<Checkout />} />
+                  <Route
+                    path="*"
+                    element={<h1>Error 404 ----- Page does not exist</h1>}
+                  />
+                </Route>
+              </Routes>
+            </Suspense>
           </CartProvider>
         </ProductsProvider>
       </AuthorizationProvider>
