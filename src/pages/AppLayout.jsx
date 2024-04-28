c0abfbf12d74d347ee031f2eb66ed2d5a4b7adc4
Kartikey Jaiswal <karan74jaiswal@gmail.com>
2024-04-29 00:02:19 +0530

    fixed tab icon and title issues

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -4,13 +4,6 @@ import { Outlet } from "react-router-dom";
 import Navigation from "../components/Navigation";
 function AppLayout() {
   useEffect(() => {
-    let link = document.querySelector("link[rel~='icon']");
-    if (!link) {
-      link = document.createElement("link");
-      link.rel = "icon";
-      document.getElementsByTagName("head")[0].appendChild(link);
-    }
-    link.href = "crown.svg";
     document.title = "crwn Clothing";
   }, []);
 
